3e5fbb17d31a686831c79fc831dce044b315e85e
KyeongYeongDEV <yeong7098@naver.com>
2022-08-06 17:30:24 +0900

    Done

--- 숫자문자열과영단어.cpp
+++ 숫자문자열과영단어.cpp
@@ -1,15 +1,54 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int solution(string s){
-    vector<char> vec;
+    string str;
 
     for(int i=0; i < s.length(); i++){
-        
+        if(s[i] >= '0' && s[i] <= '9'){
+            str+= s[i];
+        }else if(s[i] == 'z'){
+            str += '0';
+            i+=3;
+        }else if(s[i] == 'o'){
+            str += '1';
+            i+=2;
+        }else if(s[i] == 't'){
+            if(s[i+1] == 'w'){
+                str+= '2';
+                i += 2;
+            }else{
+                str += '3';
+                i+=4;
+            }
+        }else if(s[i] == 'f'){
+            if(s[i+1] == 'o'){
+                str += '4';
+                i+=3;
+            }else{
+                str += '5';
+                i+=3;
+            }
+        }else if(s[i] == 's'){
+            if(s[i+1] == 'i'){
+                str += '6';
+                i+=2;
+            }else{
+                str += '7';
+                i+=4;
+            }
+        }
+        else if(s[i] == 'e'){
+            str+='8';
+            i+=4;
+        }
+        else{
+            str += '9';
+            i+=3;
+        }
     }
 
-    return 0;
+    return stoi(str);
 }
 
 int main(){
